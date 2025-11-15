39d66cde044dd3725af4fc0d9aab23f2fac4c21b
Kostiantyn <kog@yousee.dk>
2025-11-15 15:47:45 +0200

    use DB for the task

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import "express-async-errors"; 
+import "express-async-errors";
+import sequelize from "./db/sequelize.js";
 
 import contactsRouter from "./routes/contactsRouter.js";
 
-const app = express();
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection successful");
 
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(express.json());
+    // await sequelize.sync({ alter: true });
+    // console.log("Models synchronized");
 
-app.use("/api/contacts", contactsRouter);
+    const app = express();
 
-app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
+    app.use(morgan("tiny"));
+    app.use(cors());
+    app.use(express.json());
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
+    app.use("/api/contacts", contactsRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+    app.use((_, res) => {
+      res.status(404).json({ message: "Route not found" });
+    });
+
+    app.use((err, req, res, next) => {
+      const { status = 500, message = "Server error" } = err;
+      res.status(status).json({ message });
+    });
+
+    app.listen(3000, () => {
+      console.log("Server is running. Use our API on port: 3000");
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
---
+++ db/contact.js
@@ -0,0 +1,26 @@
+import { DataTypes } from "sequelize";
+import sequelize from "./sequelize.js";
+
+export const Contact = sequelize.define("contact", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  favorite: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
